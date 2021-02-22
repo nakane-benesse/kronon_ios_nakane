9143960733bde010c7b26c826307c65c7b6491f5
nakane-benesse <nakane.benesse@gmail.com>
2021-02-22 16:49:59 +0900

    Initial Commit

---
+++ KrononIosNakane/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KrononIosNakane
+//
+//  Created by 中根悠 on 2021/02/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
