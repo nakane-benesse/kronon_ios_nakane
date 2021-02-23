d854b992ea29f4faca9568f5a49bc146c4fc7079
nakane-benesse <nakane.benesse@gmail.com>
2021-02-24 00:44:53 +0900

    textFieldのpaddingを設定しました

--- KrononIosNakane/LoginViewController.swift
+++ KrononIosNakane/LoginViewController.swift
@@ -12,6 +12,7 @@ class LoginViewController: UIViewController, UITextFieldDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        
         //これがないと角丸にならない
         inputEmail.layer.masksToBounds = true
         inputEmail.layer.cornerRadius = 20.0
@@ -22,20 +23,26 @@ class LoginViewController: UIViewController, UITextFieldDelegate {
         loginButton.layer.masksToBounds = true
         loginButton.layer.cornerRadius = 20.0
         
+        // UITextFieldの左右に余白を入れる
+        //UITextFieldクラスを継承した独自のクラスを作るのが本当は丸そうだが、よく分からないので一旦個別に指定してみる
+        let paddingView = UIView(frame: CGRect(x: 0.0, y: 0.0, width: 20.0, height: 0.0))
+        inputEmail.leftView = paddingView
+        inputEmail.leftViewMode = .always
+        inputEmail.rightView = paddingView
+        inputEmail.rightViewMode = .always
+        
+        //なぜかpaddingViewをもう一回使おうとするとフリーズするので再定義
+        let paddingView2 = UIView(frame: CGRect(x: 0.0, y: 0.0, width: 20.0, height: 0.0))
+        inputPassword.leftView = paddingView2
+        inputPassword.leftViewMode = .always
+        inputPassword.rightView = paddingView2
+        inputPassword.rightViewMode = .always
+        
+        
         inputEmail.delegate = self
         inputPassword.delegate = self
     }
-    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
     
     @IBOutlet weak var inputEmail: UITextField!
     @IBOutlet weak var inputPassword: UITextField!
@@ -50,4 +57,7 @@ class LoginViewController: UIViewController, UITextFieldDelegate {
         
         return true
     }
+    
+
+
 }
