2d588b83720d0b6d39a440dc51f0f2dcd00f6a93
nakane-benesse <nakane.benesse@gmail.com>
2021-03-28 04:51:58 +0900

    textViewでもキーボードが閉じられるようにしました。textviewがキーボードに隠れないようにはできませんでした。プルダウンはまだ未実装。

--- KrononIosNakane/CreateScheduleViewController.swift
+++ KrononIosNakane/CreateScheduleViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class CreateScheduleViewController: UIViewController {
+class CreateScheduleViewController: UIViewController , UITextFieldDelegate, UIScrollViewDelegate{
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -15,21 +15,85 @@ class CreateScheduleViewController: UIViewController {
         navigationItem.title = "予定登録"
         self.navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.black, NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: 20.0)]
         
+        inputTitle.delegate = self
+        inputPlace.delegate = self
+        //inputContent.delegate = self
+        scrollView.delegate = self
         
 
+        setupNotifications()
+        
+        //uiViewのキーボードを閉じる
+        setDismissKeyboard()
         // Do any additional setup after loading the view.
     }
     
     
 
-    /*
-    // MARK: - Navigation
+    @IBOutlet weak var inputTitle: UITextField!
+    @IBOutlet weak var inputPlace: UITextField!
+    @IBOutlet weak var inputContent: UITextView!
+    
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+    @IBOutlet weak var scrollViewBottom: NSLayoutConstraint!
+    
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func setupNotifications() {
+      //キーボードが表示される時呼ばれるNotification
+      NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillChangeFrame), name: UIResponder.keyboardWillChangeFrameNotification, object: nil)
+      //キーボードが非表示になる時呼ばれるNotification
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
     }
-    */
 
+    @objc private func keyboardWillChangeFrame(_ notification: Notification) {
+        
+        //できないよおおおおおおおおお
+//        //キーボードのサイズ
+//        guard let keyboardFrame = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue,
+//              //キーボードのアニメーション時間
+//              let duration = notification.userInfo?[UIResponder.keyboardAnimationDurationUserInfoKey] as? TimeInterval,
+//              //キーボードのアニメーション曲線
+//              let curve = notification.userInfo?[UIResponder.keyboardAnimationCurveUserInfoKey] as? UInt,
+//              //Outletで結び付けたScrollViewのBottom制約
+//              let scrollViewBottom = self.scrollViewBottom else { return }
+//
+//        //キーボードの高さ
+//        let keyboardHeight = keyboardFrame.height
+//        //Bottom制約再設定
+//        scrollViewBottom.constant = keyboardHeight
+//
+//        //アニメーションを利用してキーボードが上がるアニメーションと同じ速度でScrollViewのたBottom制約設定を適応
+//        UIView.animate(withDuration: duration, delay: 0, options: UIView.AnimationOptions(rawValue: curve), animations: {
+//          self.view.layoutIfNeeded()
+//        })
+      print("キーボード表示")
+    }
+
+    @objc private func keyboardWillHide(_ notification: Notification) {
+      print("キーボード非表示")
+    }
+    
+    //textfield用
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        //returnを押したらキーボードを閉じるように
+        textField.resignFirstResponder()
+        return true
+    }
+
+
+}
+
+extension UIViewController {
+    
+    //UIViewで、他の場所をタップした時にキーボードを閉じる
+    func setDismissKeyboard() {
+        let tapGR: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
+        tapGR.cancelsTouchesInView = false
+        self.view.addGestureRecognizer(tapGR)
+    }
+    
+    @objc func dismissKeyboard() {
+        self.view.endEditing(true)
+    }
 }
